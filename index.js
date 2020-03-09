753300d47a18af6486bf56c40a88515d861e23b2
Keith Gabriel  N. Badulis <11826401@dlsu.edu.ph>
2020-03-09 14:57:24 +0800

    routing for reservations

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const port = 3000;
 const app = express();
 
 app.use(express.static('public'));
+
 app.set('view engine', 'hbs');
 
 // hbs.registerHelper();
 
 // hbs.registerPartials();
 
-/* app.get(<url>, function(req, res) {
+app.get('(/index.html)?', function(req, res) {
+    res.render('index');
+});
 
-}); */
+app.get('/equipment(-form.html)?', function(req, res) {
+    res.render('equipment-form');
+});
+
+app.get('/locker(-form.html)?', function(req, res) {
+    res.render('locker-form');
+});
 
 app.listen(port, function () {
     console.log('Listening at port ' + port);
