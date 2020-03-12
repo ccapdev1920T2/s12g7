e62714610429dec7cce2b6e7138dd6a547344237
Ky Matias <kymatias@Kys-MacBook-Air.local>
2020-03-12 19:57:21 +0800

    Added user

---
+++ model/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    idNum: {type: String, required: true},
+    collegeName: {type: String, required: true},
+    degreeProg: {type: String, required: true},
+    contactNum: {type: String, required: true},
+    isAdmin: {type: Boolean, default:false, required: true}
+});
+
+module.exports = mongoose.model('User', User);
