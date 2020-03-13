4106fc491c4d4481f53ee74a9b368b8c010fc989
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-13 19:11:20 +0800

    added enum validation for panel lockers

--- model/panel.js
+++ model/panel.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
 const panelSchema = new mongoose.Schema({
-    type: {type: String, required: true},
-    building: {type: String, required: true},
-    floor: {type: String, required: true},
-    lockers: [{
-        number: {type: Number, required: true, unique: true},
-        status: {type: String, required: true}
-    }]
+    type: { type: String, required: true },
+    building: { type: String, required: true },
+    floor: { type: String, required: true },
+    lockers: [
+        {
+            number: { type: Number, required: true, unique: true },
+            status: { 
+                type: String, 
+                required: true, 
+                enum: ['vacant', 'occupied', 'broken', 'uncleared'] 
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('Panel', panelSchema);
