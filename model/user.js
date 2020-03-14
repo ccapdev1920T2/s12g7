ff6475d1b6f4a7b91b2e0bbd785f16e6737f553f
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-14 12:36:25 +0800

    testing db connection

--- model/user.js
+++ model/user.js
@@ -3,11 +3,23 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    middleName: { type: String, required: true },
     idNum: { type: String, required: true },
-    collegeName: { type: String, required: true },
+    college: {
+        type: String, required: true,
+        enum: [
+            'Brother Andrew Gonzalez College of Education',
+            'College of Computer Studies',
+            'College of Liberal Arts',
+            'College of Science',
+            'Gokongwei College of Engineering',
+            'Ramon V. Del Rosario College of Business',
+            'School of Economics']
+    },
     degreeProg: { type: String, required: true },
-    contactNum: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false, required: true }
+    contactNum: { type: Number, required: true },
+    isAdmin: { type: Boolean, default: false },
+    profilePicURL: {type: String, default: 'default-pic.jpg'}
 });
 
 module.exports = mongoose.model('User', userSchema);
