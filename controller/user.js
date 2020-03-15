34bb8c7bf17997f323e906f3da5769f9ff4f8ab9
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-15 23:53:44 +0800

    user login and home now follows MVC

---
+++ controller/user.js
@@ -0,0 +1,16 @@
+const User = require('../model/user');
+const passport = require('passport');
+
+exports.signin = passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/userinfo.profile', 
+        'https://www.googleapis.com/auth/userinfo.email']
+});
+
+exports.callback = passport.authenticate('google', {
+    failureRedirect: '/login'
+});
+
+exports.callback_success = function (req, res) {
+    req.session.token = req.user.token;
+    res.redirect('/');
+}
