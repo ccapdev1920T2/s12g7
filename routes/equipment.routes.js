78d2b36027f94ecde141ea27161364fce7f551b4
Ky Matias <kymatias@Kys-MacBook-Air.local>
2020-03-18 12:02:02 +0800

    created equipment.routes and equipment.controller

---
+++ routes/equipment.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const equipment_controller = require('../controller/equipment.controller');
+
+// create
+router.post('/panel', equipment_controller.equipment_create);
+
+// read
+router.get('/panel', equipment_controller.equipment_details);
+
+//update or replace TODO:
+router.put('', equipment_controller.equipment_update);
+
+// delete TODO:
+router.delete('', equipment_controller.equipment_delete);
