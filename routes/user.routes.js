899cbe45045127cd0792b2f26416902da02d69ae
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-18 21:36:59 +0800

    contact number can now be edited in the form

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const user_controller = require('../controller/user.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', user_controller.viewProfile);
+router.get('/', user_controller.profile_details);
+router.post('/', user_controller.profile_update);
 
 module.exports = router;
