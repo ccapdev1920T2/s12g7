11888076cd8cc48b1282956046852e0866edbc58
issertmg <52034895+issertmg@users.noreply.github.com>
2020-03-18 22:23:04 +0800

    updated locker status update route

--- routes/panel.routes.js
+++ routes/panel.routes.js
@@ -5,7 +5,7 @@ const panel_controller = require('../controller/panel.controller');
 
 router.post('/panel', panel_controller.panel_create);
 router.get('/panel', panel_controller.panel_details);
-router.put('/panel/:panelid/locker/:lockernumber', panel_controller.panel_update);
-router.delete('/panel/:panelid', panel_controller.panel_delete);
+router.post('/panel/update', panel_controller.panel_update);
+//router.delete('/panel/:panelid', panel_controller.panel_delete);
 
-module.exports = router;
+module.exports = router
