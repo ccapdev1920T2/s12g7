0f9cff4487054809d4081725821ca2f8ebc2b98d
Ky Matias <kymatias@Kys-MacBook-Air.local>
2020-03-18 15:32:46 +0800

    added create function in controller

--- controller/equipment.controller.js
+++ controller/equipment.controller.js
@@ -1,9 +1,24 @@
 const Equipment = require('../model/equipment.model');
 
-exports.equipment_create = 
+exports.createEquipment = async function (req, res) {
+    let equipment = new Equipment({
+        name: req.body.name,
+        quantity: req.body.quantity,
+        available: req.body.available
+        // ,imageURL: req.body.imageURL TODO:
+    });
 
-exports.equipment_details = 
+    await panel.save(function (err) {
+        if (err) {
+            console.log('Error writing to db');
+        } else {
+            console.log('success');
+        }
+    });
+};
 
-exports.equipment_update = 
+/* exports.equipment_details = ;
 
-exports.equipment_delete = 
+exports.equipment_update = ;
+
+exports.equipment_delete = ; */
