8301680c6e918a0ad663bbb05cda9b809133b084
Ky Matias <kymatias@Kys-MacBook-Air.local>
2020-03-18 20:05:25 +0800

    removed _itemID from equipment.model

--- model/equipment.model.js
+++ model/equipment.model.js
@@ -4,9 +4,7 @@ const equipmentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     quantity: { type: Number, default: 0 },
     available: { type: Number, default: 0 }
-    /* ,
-    imageURL: { type: String, required: true },
-    _itemID: { type: mongoose.Schema.Types.ObjectId, required: true, default: null } */
+    //,imageURL: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Equipment', equipmentSchema);
