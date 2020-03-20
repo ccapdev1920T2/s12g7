017cf8e9a4bbb7f0f90c9d3771e5e2ca0fae2005
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-21 00:15:40 +0800

    my reservations page can now show all reservatios

--- controller/panel.controller.js
+++ controller/panel.controller.js
@@ -100,7 +100,8 @@ exports.panel_details = async function(req, res) {
                     active: { active_manage_lockers: true },
                     sidebarData: { 
                         dp: req.session.passport.user.profile.photos[0].value,
-                        name: req.session.passport.user.profile.displayName
+                        name: req.session.passport.user.profile.displayName,
+                        idNum: req.session.idNum
                     }
                 });
             }
