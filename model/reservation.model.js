756071c6b233ee14f0882d8d0db9aa26c9b500da
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-21 00:15:40 +0800

    my reservations page can now show all reservatios

--- model/reservation.model.js
+++ model/reservation.model.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 
 const reservationSchema = new mongoose.Schema({
-    userID: {type: Number, required: true},
+    title: String,
+    userID: { type: Number, required: true },
     reservationType: { type: String, enum: ['locker', 'equipment'] },
-    date: { type: Date, default: Date.now },
-    status: { 
-        type: String, 
+    itemID: {type: mongoose.Schema.Types.ObjectId, required: false},
+    dateCreated: { type: Date, default: Date.now() },
+    status: {
+        type: String,
         enum: [
-            'Pending', 
-            'For Pickup', 
-            'To Pay', 
-            'On Rent', 
-            'Denied', 
-            'Uncleared', 
+            'Pending',
+            'For Pickup',
+            'To Pay',
+            'On Rent',
+            'Denied',
+            'Uncleared',
             'Returned'
         ]
     },
     description: String,
-    remarks: String
+    remarks: {type: String, default: 'N/A'},
+    penalty: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Reservation', reservationSchema);
