de0f17392d9b111924ad9172f0451b61689b13cd
issertmg <issertmg@gmail.com>
2020-03-23 15:25:41 +0800

    equipment reserve (in progress)

--- routes/reserve.routes.js
+++ routes/reserve.routes.js
@@ -8,6 +8,7 @@ router.get('/', index_controller.home);
 router.get('/locker', reserve_controller.locker);
 router.get('/equipment', reserve_controller.equipment);
 
-router.post('/locker', reserve_controller.locker_reserve);
+router.post('/locker', reserve_controller.reserve_locker);
+router.post('/equipment', reserve_controller.reserve_equipment);
 
 module.exports = router;
