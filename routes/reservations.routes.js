1f6015de9a3fcacfca015387fdadb432f9cad0a3
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-03-27 16:34:08 +0800

    added approve and deny modals

--- routes/reservations.routes.js
+++ routes/reservations.routes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get('/', reservations_controller.myReservations);
 
 router.get('/manage', reservations_controller.reservation_details);
+// router.post('/manage', reservations_controller.reservation_create);
 
-// router.post('/manage/create', reservations_controller.reservation_create);
-// router.post('/manage/update', reservations_controller.reservation_update);
+router.post('/manage/update', reservations_controller.reservation_update);
 router.post('/manage/delete', reservations_controller.reservation_delete);
 
 module.exports = router;
