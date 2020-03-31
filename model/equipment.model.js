c7304efb6584061732ed96e7210a97efbb712d7b
issertmg <52034895+issertmg@users.noreply.github.com>
2020-03-31 21:32:36 +0800

    changed attribute

--- model/equipment.model.js
+++ model/equipment.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const equipmentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     quantity: { type: Number, default: 0 },
-    available: { type: Number, default: 0 },
+    onRent: { type: Number, default: 0 },
     imageURL: { type: String, required: true }
 });
 
